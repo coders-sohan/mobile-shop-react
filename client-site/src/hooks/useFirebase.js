27bba7838a6fe19e7a2b5227fe8cc327d402d1bf
coders-sohan <coders.sohan.0216@gmail.com>
2022-04-09 03:39:10 +0600

    mongoDB server install and firebase intigraded

---
+++ client-site/src/hooks/useFirebase.js
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import initializeFirebase from "../components/Firebase/Firebase.init";
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	GoogleAuthProvider,
+	signInWithPopup,
+	updateProfile,
+	sendPasswordResetEmail,
+	signOut,
+} from "firebase/auth";
+
+// initialize firebase
+initializeFirebase();
+
+const useFirebase = () => {
+	const [isLoading, setIsLoading] = useState(true);
+	const [user, setUser] = useState({});
+	const [error, setError] = useState("");
+
+	const auth = getAuth();
+	const googleProvider = new GoogleAuthProvider();
+
+	const registerUser = (email, password, name, history) => {
+		setIsLoading(true);
+		createUserWithEmailAndPassword(auth, email, password)
+			.then(() => {
+				setError("");
+				const newUser = { email, displayName: name };
+				setUser(newUser);
+				// send name to firebases
+				updateProfile(auth.currentUser, {
+					displayName: name,
+					photoURL: "https://dummyimage.com/100/bababa/000000",
+				})
+					.then(() => {
+						setError("");
+					})
+					.catch((error) => {
+						setError(error.message);
+					});
+				history.replace("/");
+			})
+			.catch((error) => {
+				setError(error.message);
+			})
+			.finally(() => setIsLoading(false));
+	};
+
+	const loginUser = (email, password, location, history) => {
+		setIsLoading(true);
+		signInWithEmailAndPassword(auth, email, password)
+			.then(() => {
+				const destination = location?.state?.from || "/";
+				history.replace(destination);
+				setError("");
+			})
+			.catch((error) => {
+				setError(error.message);
+			})
+			.finally(() => setIsLoading(false));
+	};
+
+	const googleSignIn = (location, history) => {
+		setIsLoading(true);
+		signInWithPopup(auth, googleProvider)
+			.then((result) => {
+				// The signed-in user info.
+				// const user = result.user;
+				const destination = location?.state?.from || "/";
+				history.replace(destination);
+				setError("");
+			})
+			.catch((error) => {
+				setError(error.message);
+			})
+			.finally(() => setIsLoading(false));
+	};
+
+	const forgotPassword = (email, location, history) => {
+		setIsLoading(true);
+		sendPasswordResetEmail(auth, email)
+			.then(() => {
+				const destination = location?.state?.from || "/";
+				history.replace(destination);
+				setError("");
+			})
+			.catch((error) => {
+				setError(error.message);
+			})
+			.finally(() => setIsLoading(false));
+	};
+
+	const logout = () => {
+		setIsLoading(true);
+		signOut(auth)
+			.then(() => {
+				// Sign-out successful.
+				setError("");
+			})
+			.catch((error) => {
+				setError(error.message);
+			})
+			.finally(() => setIsLoading(false));
+	};
+
+	return {
+		user,
+		error,
+		isLoading,
+		registerUser,
+		loginUser,
+		googleSignIn,
+		forgotPassword,
+		logout,
+	};
+};
+
+export default useFirebase;
